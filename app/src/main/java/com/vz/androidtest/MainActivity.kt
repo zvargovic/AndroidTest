1462b2e1b9b7f0a9ab003e37abfec0bdd9f83af4
zvargovic <zvargovic@gmail.com>
2023-06-04 13:40:47 +0200

    Initial commit

---
+++ app/src/main/java/com/vz/androidtest/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.vz.androidtest
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.vz.androidtest.ui.theme.AndroidTestTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
